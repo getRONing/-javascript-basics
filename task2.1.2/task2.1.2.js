ac75bcfa38c6772769fc8b6ccfcf13f060c719a6
getRON ing <trosenkooleg@gmail.com>
2025-03-06 17:34:39 +0200

    Завдання 5
    Було змінено знаки більше та менше, я перепутав їх трошки, і добавлено дужечки для настрою:)

---
+++ task2.1.2/task2.1.2.js
@@ -0,0 +1,19 @@
+let randomNumber = Math.floor(Math.random() * 101);
+let userNumber;
+let attempts = 0;
+
+do {
+    userNumber =  Number(prompt("назви число від 0 до 100"));
+    attempts++;
+
+
+    if (isNaN(userNumber)) {
+        alert("Ведіть число ;)");
+    } else if(userNumber > randomNumber) {
+        alert("ваше число менше ))")
+    } else if(userNumber < randomNumber) {
+        alert("ваше число більше ))");
+    } else {
+        alert(`Вітаємо! Ви вгадали число ${randomNumber} 🎉`);
+    }
+} while (userNumber !== randomNumber);
